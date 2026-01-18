3b9c0a9c4b716579a2e62412846cb9dd36bcf389
Plutonium3233 <111406634+Plutonium3233@users.noreply.github.com>
2026-01-17 20:55:36 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="swipe-racer-v1";
+const CACHE="swipe-racer-v2";
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
